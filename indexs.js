94bc5ccd8f2bdd717ebc48ed52c0726850430f6d
Ryandra Rama Syahputra <20390100005@dinamika.ac.id>
2022-06-17 20:35:47 +0700

    initial commit

---
+++ indexs.js
@@ -0,0 +1,84 @@
+const Discord = require("discord.js")
+const dotenv = require("dotenv")
+const { REST } = require("@discordjs/rest")
+const { Routes } = require("discord-api-types/v9")
+const fs = require("fs")
+const { Player } = require("discord-player")
+const mongoose = require("mongoose")
+const testSchema = require("./test-schema.js")
+
+dotenv.config()
+const TOKEN = process.env.TOKEN
+const LOAD_SLASH = process.argv[2] == "load"
+const CLIENT_ID = "985545689465507940"
+const GUILD_ID = "979061082909966426"
+
+
+const client = new Discord.Client({
+    intents: [
+        "GUILDS",
+        "GUILD_VOICE_STATES"
+    ]
+})
+
+client.on('ready', async () => {
+    await mongoose.connect(process.env.MONGO_URI, {
+        keepAlive: true,
+    })
+    setTimeout(async() => {
+        await new testSchema({
+            message: "Hello World"
+        }).save()
+    }, 1000)
+})
+
+client.slashcommands = new Discord.Collection()
+client.player = new Player(client, {
+    ytdlOptions: {
+        quality: "highestaudio",
+        highWaterMark: 1 << 25
+    }
+})
+
+let commands = []
+
+const slashFiles = fs.readdirSync("./slash").filter(file => file.endsWith(".js"))
+for (const file of slashFiles){
+    const slashcmd = require(`./slash/${file}`)
+    client.slashcommands.set(slashcmd.data.name, slashcmd)
+    if (LOAD_SLASH) commands.push(slashcmd.data.toJSON())
+}
+
+if (LOAD_SLASH) {
+    const rest = new REST({ version: "9" }).setToken(TOKEN)
+    console.log("Deploying slash commands")
+    rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {body: commands})
+    .then(() => {
+        console.log("Successfully loaded")
+        process.exit(0)
+    })
+    .catch((err) => {
+        if (err){
+            console.log(err)
+            process.exit(1)
+        }
+    })
+}
+else {
+    client.on("ready", () => {
+        console.log(`Logged in as ${client.user.tag}`)
+    })
+    client.on("interactionCreate", (interaction) => {
+        async function handleCommand() {
+            if (!interaction.isCommand()) return
+
+            const slashcmd = client.slashcommands.get(interaction.commandName)
+            if (!slashcmd) interaction.reply("Not a valid slash command")
+
+            await interaction.deferReply()
+            await slashcmd.run({ client, interaction })
+        }
+        handleCommand()
+    })
+    client.login(TOKEN)
+}
